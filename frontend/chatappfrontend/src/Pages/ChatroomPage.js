df8d9b6e76295fe9bd57c6f60f73ce5ae1cc4993
Archit <archit.hadge@gmail.com>
2021-03-10 00:23:15 +0530

    Styling

--- frontend/chatappfrontend/src/Pages/ChatroomPage.js
+++ frontend/chatappfrontend/src/Pages/ChatroomPage.js
@@ -4,50 +4,50 @@ import io from 'socket.io-client';
 import axios from 'axios';
 import Message from './Message.js';
 
-const ChatroomPage = ({match,socket}) => {
-    const chatroomId=match.params.id;
-    const [messages,setMessages]=React.useState([]);
-    const [messagesFromDB,setMessagesDB]=React.useState([]);
-    const messageRef=React.useRef();
+const ChatroomPage = ({ match, socket }) => {
+    const chatroomId = match.params.id;
+    const [messages, setMessages] = React.useState([]);
+    const [messagesFromDB, setMessagesDB] = React.useState([]);
+    const messageRef = React.useRef();
 
-    const getMessagesFromDB=()=>{
-        axios.post('http://localhost:8000/chatroom/messages',{
-            chatroomId:chatroomId
+    const getMessagesFromDB = () => {
+        axios.post('http://localhost:8000/chatroom/messages', {
+            chatroomId: chatroomId
         },
-        {
-            headers: {
-                Authorization: "Bearer " + localStorage.getItem("Token")
-            }
-        }).then((response)=>{
-            console.log(response.data);
-            setMessagesDB(response.data);
-        }).catch((error)=>{
-            console.log("Error occured ..!",error);
-        })
+            {
+                headers: {
+                    Authorization: "Bearer " + localStorage.getItem("Token")
+                }
+            }).then((response) => {
+                console.log(response.data);
+                setMessagesDB(response.data);
+            }).catch((error) => {
+                console.log("Error occured ..!", error);
+            })
     }
 
-    const sendMessage=()=>{
-        console.log("Messages",messages);
-        
-        if(socket){
-            socket.emit('chatroomMessage',{
+    const sendMessage = () => {
+        console.log("Messages", messages);
+
+        if (socket) {
+            socket.emit('chatroomMessage', {
                 chatroomId,
-                message:messageRef.current.value
+                message: messageRef.current.value
             })
         }
     }
 
-    React.useEffect(()=>{
+    React.useEffect(() => {
         console.log("Setting up");
         getMessagesFromDB();
-        socket.once('newMessage',(message)=>{
+        socket.once('newMessage', (message) => {
             console.log("inside new msg");
-            const newMessages=[...messages,message];
+            const newMessages = [...messages, message];
             setMessages(newMessages);
         })
-    },[])
-    React.useEffect(()=>{
-        socket.emit('joinRoom',{
+    }, [])
+    React.useEffect(() => {
+        socket.emit('joinRoom', {
             chatroomId
         })
 
@@ -56,13 +56,13 @@ const ChatroomPage = ({match,socket}) => {
         //     setMessages([...messages,message]);
         // })
 
-        return ()=>{
-            socket.emit('leaveRoom',{
+        return () => {
+            socket.emit('leaveRoom', {
                 chatroomId
             })
         }
-    },[])
-    
+    }, [])
+
     // const socket=io('http://localhost:8000',{
     //     query:{
     //         token=localStorage.getItem("Token")
@@ -71,20 +71,25 @@ const ChatroomPage = ({match,socket}) => {
     return (
         <div>
             Chatroom Page
-            <input type='text' ref={messageRef}></input>
-            <button onClick={sendMessage} ></button>
-            <button onClick={getMessagesFromDB}>Test</button>
+            
+            {/* <button onClick={getMessagesFromDB}>Test</button> */}
+            <div className='messages'>
             <div>
-            {messagesFromDB.map((message,i)=>(
+                {messagesFromDB.map((message, i) => (
                     // <div key={i}>{message.name}:---{message.message}</div>
                     <Message key={i} user={message._id} msg={message.message}></Message>
                 ))}
             </div>
             <div>
-                {messages.map((message,i)=>(
+                {messages.map((message, i) => (
                     // <div key={i}>{message.name}:---{message.message}</div>
                     <Message key={i} user={message.userId} msg={message.message}></Message>
                 ))}
+            </div>
+            </div>
+            <div>
+            <input type='text' ref={messageRef}></input>
+            <button onClick={sendMessage} ></button>
             </div>
         </div>
     );
