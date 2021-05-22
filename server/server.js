6f6dcdc7e990404a97057aa8a18c73682259816f
Archit <archit.hadge@gmail.com>
2021-05-23 00:31:14 +0530

    Stopped tracking client pages,server models and controllers

--- server/server.js
+++ server/server.js
@@ -19,7 +19,6 @@ require("./models/User");
 require("./models/Chatroom");
 require("./models/ChatroomMessage");
 require("./models/PersonalMessage");
-
 const Chatroom = require("./models/Chatroom");
 const ChatroomMessage = mongoose.model("ChatroomMessage");
 const User = mongoose.model("User");
@@ -122,74 +121,3 @@ io.on("connection", (socket) => {
   });
 });
 
-// io.use(async (socket,next)=>{
-//     try{
-//         const token=socket.handshake.query.token;
-//         const payload=await jwt.verify(token,process.env.SECRET);
-//         socket.userId=payload.id;
-//         next();
-//     }catch(err){
-
-//     }
-// });
-
-// io.on('connection',(socket)=>{
-//     // const token=socket.handshake.query.token;
-
-//     // console.log("All sockets->",io.sockets.sockets)
-//     // l.push(socket.id);
-//     // console.log("All sockets ",l);
-//     // c=c+1;
-//     // console.log('c',c);
-//     console.log("Connected "+socket.id);
-
-//     socket.on('disconnect',()=>{
-//         socket.disconnect();
-//         // c=c-1;
-//     // console.log('c',c);
-//         console.log("Disconnected "+socket.id);
-//     })
-
-//     socket.on('joinRoom',({chatroomId})=>{
-//         socket.join(chatroomId);
-//         console.log(socket.userId+" has joined chatroom "+chatroomId);
-//     })
-
-//     socket.on('leaveRoom',({chatroomId})=>{
-//         socket.leave(chatroomId);
-//         console.log(socket.userId+" has left chatroom "+chatroomId);
-//     })
-
-//     socket.on('chatroomMessage',async ({chatroomId,message})=>{
-//         console.log('Message arrived< ',message," >from ",socket.userId);
-//         const user=await User.findOne({_id:socket.userId});
-//         const msg=new ChatroomMessage({
-//             chatroom:chatroomId,
-//             user:socket.userId,
-//             message:message
-//         })
-//         io.to(chatroomId).emit('newMessage',{
-//             message:message,
-//             name:user.name,
-//             userId:user._id,
-//         })
-//         await msg.save();
-//     })
-
-//     socket.on('personalMessage',async ({receiverId,chatroom,message})=>{
-//         console.log('Message arrived< ',message," >from ",socket.userId);
-//         const user=await User.findOne({_id:socket.userId});
-//         const msg=new PersonalMessage({
-//             sender:socket.userId,
-//             receiver:receiverId,
-//             chatroom:chatroom,
-//             message:message
-//         })
-//         io.to(chatroom).emit('newMessage',{
-//             message:message,
-//             name:user.name,
-//             userId:user._id,
-//         })
-//         await msg.save();
-//     })
-// })
