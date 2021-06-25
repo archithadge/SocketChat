4d84be58bae28894e608faa40c782b5bff2f4a5a
Archit <archit.hadge@gmail.com>
2021-06-25 23:03:38 +0530

    Image link stored to database in User's document.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,51 +1,62 @@
-const mongoose=require('mongoose');
-const User=mongoose.model("User");
-const sha256=require('js-sha256');
-const jwt=require('jwt-then');
+const mongoose = require('mongoose');
+const User = mongoose.model("User");
+const sha256 = require('js-sha256');
+const jwt = require('jwt-then');
 
 
 //Register new users
-exports.register=async (req,res)=>{
-    const firstname=req.body.firstname;
-    const lastname=req.body.lastname;
-    const email=req.body.email;
-    const bio=req.body.bio;
-    const password=req.body.password;
-    const profilephoto=req.body.profilephoto;
+exports.register = async (req, res) => {
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    const email = req.body.email;
+    const bio = req.body.bio;
+    const password = req.body.password;
+    const profilephoto = req.body.profilephoto;
 
-    const user=new User({firstname,lastname,email,bio,password:sha256(password+process.env.SALT)});
-    await user.save();
+    const user = new User({
+        firstname,
+        lastname,
+        email,
+        bio,
+        password: sha256(password + process.env.SALT),
+        profilephoto: 'http://localhost:8000/profilepics/' + req.file.filename
+    });
+    await user.save((err, data) => {
+        console.log(data._id);
+    });
+
+    console.log(req.file.filename);
 
     res.json({
-        message:"User "+firstname+" "+lastname+" registered successfully..!"
+        message: "User " + firstname + " " + lastname + " registered successfully..!"
     });
 
 };
 
 //Login new user
-exports.login=async (req,res)=>{
-    const email=req.body.email;
-    const password=req.body.password;
+exports.login = async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
 
-    const user=await User.findOne({
+    const user = await User.findOne({
         email,
-        password:sha256(password+process.env.SALT)
+        password: sha256(password + process.env.SALT)
     });
 
-    if(!user) throw "User doesn't exists";
+    if (!user) throw "User doesn't exists";
 
-    const token=await jwt.sign({id:user.id},process.env.SECRET);
+    const token = await jwt.sign({ id: user.id }, process.env.SECRET);
 
     res.json({
-        message:"User logged in successfully",
-        token:token,
-        uid:user._id
+        message: "User logged in successfully",
+        token: token,
+        uid: user._id
     })
 
 }
 
 //Get all users
-exports.getAllUsers=async (req,res)=>{
-    const users=await User.find({}).select(['_id','name'])
+exports.getAllUsers = async (req, res) => {
+    const users = await User.find({}).select(['_id', 'name'])
     res.json(users);
 }
